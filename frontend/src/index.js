cfd855a10792317f5d91e5f139af161e851e209e
lilly0305 <yhklilly0305@gmail.com>
2022-06-14 23:39:35 +0900

    router added at app.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from './store/ReduxStore';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "*" element={<App />}/>
+      </Routes>
+    </BrowserRouter>
   </Provider>,
 
   document.getElementById("root")
